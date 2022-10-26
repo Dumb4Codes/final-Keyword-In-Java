8e09d8a9d0e39f34f5c1edf1a57cac29d81df6a8
Youvraj Singh <114943098+Dumb4Codes@users.noreply.github.com>
2022-10-26 12:43:53 +0530

    Add files via upload

---
+++ Z17_FinalKeyword.java
@@ -0,0 +1,17 @@
+
+public class Z17_FinalKeyword {
+
+	public static void main(String[] args) {
+
+
+		// final = cannot be changed when used
+		
+				final double pi = 3.1486846;
+				
+				// pi = 4;
+				
+				System.out.println(pi);
+
+	}
+
+}
